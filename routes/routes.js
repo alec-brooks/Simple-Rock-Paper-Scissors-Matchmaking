3f6b97077057b42f911a915e72cf0c8df754f5a4
Alec Brooks <abrooks@thoughtworks.com>
2017-04-08 12:11:35 +1000

    Persist throws

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,19 @@
 const choices = ['rock', 'paper', 'scissors'];
+
+const attemptToThrow = (throws) => (req, res) => (
+  res.send(
+    choices.includes(req.body.choice)
+    ? throws.push(req.body) && `${req.body.name} throws ${req.body.choice}`
+    : { status: 'error', message: `Please select one of the following: ${choices}` }
+  )
+);
+
 const appRouter = (app) => {
+  const throws = [];
+
   app.get('/', (req, res) => res.send('Hello World'));
-  app.post('/rps', (req, res) =>
-    res.send(
-      choices.includes(req.body.choice)
-      ? `${req.body.name} throws ${req.body.choice}`
-      : { status: 'error', message: `Please select one of the following: ${choices}` }
-    )
-  );
+  app.get('/throws', (req, res) => res.send(throws));
+  app.post('/rps', attemptToThrow(throws));
 };
 
 module.exports = appRouter;
